5e26f4fa17a61df9ffa447da8fb7aad19b131b2f
--add <andrewksmoker@gmail.com>
2021-02-01 12:26:56 -0500

    commit tree bb04a999fec1b4caf5a73435ed351178bbe49d40

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(src, mapping) {
+    let r = []
+
+    for (let i = 0; i < src.length; i++) {
+        let theElement = src[i]
+        r.push(mapping(theElement))
+    }
+    return r;
+}
+
+function reduce(src, cb, starting) {
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (i; i < src.length; i++) {
+        r = cb(src[i], r)
+    }
+
+    return r;
+}
